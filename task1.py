0502e69dd240ea26a1c2c5a896594c8ea9bd76cf
dwb1660322730 <146362398+dwb1660322730@users.noreply.github.com>
2023-09-29 23:53:49 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+def maze_build():
+    maze = [[0 for j in range(10)] for i in range(10)]
+    # 随机选择入口和出口
+    while(1):
+        import random
+        entrance = random.choice([(0, i) for i in range(1, 9)] + [(i, 0) for i in range(1, 9)] + [(i, 9) for i in range(1, 9)])
+        exit = random.choice([(0, i) for i in range(1, 9)] + [(i, 0) for i in range(1, 9)] + [(i, 9) for i in range(1, 9)])
+        if(entrance[0]!=exit[0] and entrance[1]!=exit[1]):
+            maze[entrance[0]][entrance[1]] = 2
+            maze[exit[0]][exit[1]] = 8
+            break
+    for i in range(1, 9):
+        for j in range(1, 9):
+            maze[i][j] = random.randint(0, 1)
+    return maze
+if __name__ == "__main__":
+    print(maze_build())
